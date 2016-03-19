59f200c3a07b6ef1a75b2279d2c5549f87bc19c4
Víctor Mas <victor.mas@est.fib.upc.edu>
2016-03-19 16:49:45 +0100

    Customizable colors

--- js/board.js
+++ js/board.js
@@ -3,16 +3,22 @@
 /* global THREE */
 /* exported Board */
 
-function Board (numTilesX, numTilesY, tileSize) {
+function Board (numTilesX, numTilesY, tileSize, color0, color1) {
+    this.color0 = color0 || new THREE.Vector4(0, 0, 0, 1);
+    this.color1 = color1 || new THREE.Vector4(1, 1, 1, 1);
+
     this.mesh = new THREE.Mesh(
         new THREE.PlaneGeometry(numTilesX, numTilesY, tileSize, tileSize),
         // new THREE.MeshLambertMaterial({color: 0xceceff, wireframe: false})
         new THREE.ShaderMaterial({
             uniforms: {
-                N: { type: 'f', value: 8.0 }
+                N: { type: 'f', value: tileSize },
+                color0: { type: 'v4', value: this.color0 },
+                color1: { type: 'v4', value: this.color1 }
             },
             // attributes: {},
             // defines: {},
+            // wireframe: true,
 
             vertexShader: [
                 'varying vec2 vUv;',
@@ -27,17 +33,29 @@ function Board (numTilesX, numTilesY, tileSize) {
 
             fragmentShader: [
                 'uniform float N;',
+                'uniform vec4 color0;',
+                'uniform vec4 color1;',
                 'varying vec2 vUv;',
                 '',
                 'vec4 checker(vec2 tex, vec4 color0, vec4 color1) {',
-                '    if ( mod(floor(tex.s*N), 2.0) + mod(floor(tex.t*N), 2.0) == 1.0 )',
+                // '    float intra_pos = (tex.s - floor(tex.s*N) / N) * N;',
+                '    if ( mod(floor(tex.s*N), 2.0) + mod(floor(tex.t*N), 2.0) == 1.0 ) {',
+                // '        if (intra_pos <= 0.1)',
+                // '            return mix(color0, color1, 0.5 + intra_pos*5.0);',
+                // '        else if (intra_pos >= 0.9)',
+                // '            return mix(color0, color1, intra_pos - 0.9);',
                 '        return color0;',
-                '    else',
+                '    } else {',
+                // '        if (intra_pos <= 0.1)',
+                // '            return mix(color1, color0, 0.5 + intra_pos*5.0);',
+                // '        else if (intra_pos >= 0.9)',
+                // '            return mix(color1, color0, intra_pos - 0.9);',
                 '        return color1;',
+                '    }',
                 '}',
                 '',
                 'void main() {',
-                '    gl_FragColor = checker(vUv, vec4(0, 0, 0, 1), vec4(1, 1, 1, 1));',
+                '    gl_FragColor = checker(vUv, color0, color1);',
                 '}'
             ].join('\n')
         })
@@ -45,5 +63,13 @@ function Board (numTilesX, numTilesY, tileSize) {
 
     this.render = function (scene) {
         scene.add(this.mesh);
+    };
+
+    this.updateColor0 = function (r, g, b) {
+        this.color0.set(r, g, b);
+    };
+
+    this.updateColor1 = function (r, g, b) {
+        this.color1.set(r, g, b);
     };
 }
--- js/main.js
+++ js/main.js
@@ -4,11 +4,19 @@
 /* exported app */
 
 var app = {
-    main: function (id_canvas_container) {
-        this.canvas = document.getElementById(id_canvas_container);
-        this.board = new Board(40, 40, 10);
+    main: function (idCanvasContainer) {
+        var convertColor = function (c) {
+            return c.match(/[a-z0-9]{2}/gi).map(function (e) { return Number.parseInt(e, 16) / 255.0; });
+        };
+        this.canvasContainer = document.getElementById(idCanvasContainer);
+        var col1 = convertColor('#00bfff');
+        var col2 = convertColor('#0080ff');
+
+        this.board = new Board(40, 40, 10, new THREE.Vector3(col1[0], col1[1], col1[2]), new THREE.Vector3(col2[0], col2[1], col2[2]));
         this.initScene();
+
         this.board.render(this.scene);
+
         this.render.call(this);
         var that = this;
         window.addEventListener('resize', function () {
@@ -21,12 +29,12 @@ var app = {
         this.renderer = new THREE.WebGLRenderer( { antialias: true } );
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.renderer.setClearColor( 0xAAAAAA );
-        this.canvas.appendChild( this.renderer.domElement );
+        this.canvasContainer.appendChild( this.renderer.domElement );
         // Scene, camera, clock setup
         this.clock = new THREE.Clock();
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(60.0, window.innerWidth / window.innerHeight, 10*Math.SQRT2, 80*Math.SQRT2);
-        this.camera.position.set( 0, 40*Math.SQRT2, 20 );
+        this.camera = new THREE.PerspectiveCamera(50.0, window.innerWidth / window.innerHeight, 10*Math.SQRT2, 80*Math.SQRT2);
+        this.camera.position.set( 0, -40*Math.SQRT2, 20 );
         this.cameraControls = new THREE.OrbitAndPanControls( this.camera, this.renderer.domElement );
         this.cameraControls.target.set( 0, 0, 0 );
         this.camera.updateProjectionMatrix();
